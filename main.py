4ff1b9bd0b96fcf47b4dd1e05caae7f41c7eb4be
cbolton56 <cbolton56@users.noreply.github.com>
2021-11-21 20:03:09 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from typing import Optional
+from fastapi import FastAPI
+from fastapi import Body
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str 
+    published: bool = True
+    rating: Optional[int]  = None
+
+@app.get("/")
+async def root():
+    return{"message":"Hello World"}
+
+@app.get("/") 
+def get_post(): 
+    return {"data":"This is your posts"}
+
+@app.post("/createposts")
+def create_posts(new_post: Post):
+    print(new_post)
+    print(new_post.title)
+    print(new_post.content)
+    return {"data":"new post"} 
