aade9c3815b461d2fb46c435c955d9c097f6bfb5
cbolton56 <cbolton56@users.noreply.github.com>
2021-11-21 21:54:30 -0800

    added in mem db, creating endpoint to look up post by id

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Optional
 from fastapi import FastAPI
 from fastapi import Body
 from pydantic import BaseModel
+import uuid 
 
 app = FastAPI()
 
@@ -11,18 +12,35 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int]  = None
 
+# To store posts in memory
+# Each post is a dict object
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": "1"}, {"title": "title of post 2", "content":"i'm hungry", "id":"2"}]
+
 @app.get("/")
 async def root():
     return{"message":"Hello World"}
 
-@app.get("/") 
-def get_post(): 
-    return {"data":"This is your posts"}
+@app.get("/posts") 
+def get_posts(): 
+    return {"data": my_posts}
+
+@app.get("/posts/{id}")
+def get_post(id):
+    p = None
+    for post in my_posts: 
+        if post["id"] == id: 
+            p = post
+    if p:
+        return {"post": p} 
+    else: 
+        return "No post found with given ID."
 
 @app.post("/posts")
 def create_posts(new_post: Post):
-    print(new_post)
-    print(new_post.title)
-    print(new_post.content)
+    new_id = str(uuid.uuid4())
+    post_dict = new_post.dict()
+    post_dict["id"] = new_id
+    my_posts.append(post_dict)
     return {"data": new_post} 
-    
+
+
